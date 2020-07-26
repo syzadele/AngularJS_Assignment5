7b89606d81291fec0bd232b97dfecedec9717981
Adele <syzadele@outlook.com>
2020-07-26 15:57:57 +0200

    correction 6

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://syzadele-course5.herokuapp.com/')
 .constant('MyApiPath', 'https://syzadele-course5.herokuapp.com/menu_items.json')
 .config(config);
 
